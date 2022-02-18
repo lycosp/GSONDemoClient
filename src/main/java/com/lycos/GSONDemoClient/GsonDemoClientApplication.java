d619cc30d36f8f22cc1e6ad92f90355a826013a4
lycosp <41700905+lycosp@users.noreply.github.com>
2022-02-17 22:56:38 -0500

    Initial commit

---
+++ src/main/java/com/lycos/GSONDemoClient/GsonDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.lycos.GSONDemoClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonDemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsonDemoClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lycos/GSONDemoClient/GsonDemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lycos.GSONDemoClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsonDemoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
