636d02e8324f8ea12817f5c4d73274b7212b00ea
lycosp <41700905+lycosp@users.noreply.github.com>
2022-02-17 23:33:37 -0500

    init

---
+++ src/main/java/com/lycos/GSONDemoClient/freemarker/template/Configuration.java
@@ -0,0 +1,8 @@
+package com.lycos.GSONDemoClient.freemarker.template;
+
+public class Configuration {
+
+    public Configuration() {
+        freemarker.template.Configuration configuration = new freemarker.template.Configuration();
+    }
+}
